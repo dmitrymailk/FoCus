6982142c6e39cc1c77fec090190e95da555efb82
dmitrymailk <dmitrymail_k@mail.ru>
2022-09-10 03:23:14 +0300

    train only bart

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 #Train the model only with LM
 #CUDA_VISIBLE_DEVICES=4 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --flag E2_L10_LM --ps_coef 0 --kn_coef 0 --model_name GPT2 --incontext > train_log_focus/e2_L20_LM_gpt.log &
-#CUDA_VISIBLE_DEVICES=5 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --flag E2_L10_LM --ps_coef 0 --kn_coef 0 --model_name BART --incontext > train_log_focus/e2_L20_LM_bart.log &
+CUDA_VISIBLE_DEVICES=0 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --flag E2_L10_LM --ps_coef 0 --kn_coef 0 --model_name BART --incontext --wandb --train_batch_size=2 > train_log_focus/e2_L20_LM_bart.log &
 #CUDA_VISIBLE_DEVICES=6 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --flag E2_L10_LM --ps_coef 0 --kn_coef 0 --model_name transformer-decoder --incontext > train_log_focus/e2_L20_LM_decoder.log &
 #CUDA_VISIBLE_DEVICES=7 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --flag E2_L10_LM --ps_coef 0 --kn_coef 0 --model_name transformer-encdec --incontext > train_log_focus/e2_L20_LM_encdec.log &
 
@@ -19,7 +19,7 @@
 
 #Train the model w/o Persona Selection
 # CUDA_VISIBLE_DEVICES=0 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --ps_coef 0 --flag E2_L10_WO_PS --model_name GPT2 --incontext > train_log_focus/e2_L20_gpt_WO_PS.log &
-CUDA_VISIBLE_DEVICES=0 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --ps_coef 0 --flag E2_L10_WO_PS --model_name BART --incontext --wandb --train_batch_size=2 > train_log_focus/e2_L20_bart_WO_PS.log &
+# CUDA_VISIBLE_DEVICES=0 nohup python train_focus.py --n_epochs 2 --lm_coef 10 --ps_coef 0 --flag E2_L10_WO_PS --model_name BART --incontext --wandb --train_batch_size=2 > train_log_focus/e2_L20_bart_WO_PS.log &
 
 echo
 
